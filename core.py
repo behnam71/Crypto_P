3550b3e9f536b8c6ec5dc9f1519d92ae86e3926b
behnam heydari <behnamheydari.7.21@gmail.com>
2021-06-06 14:13:10 +0430

    Update core.py

--- core.py
+++ core.py
@@ -1,113 +1,190 @@
 import ray
-import numpy as np
-
 from ray import tune
 from ray.tune.registry import register_env
+import ray.rllib.agents.ppo as ppo
 
-import tensortrade.env.default as default
+import pandas as pd
+import ta
+from IPython.display import display
 
-from tensortrade.feed.core import DataFeed, Stream
-from tensortrade.oms.instruments import Instrument
+
+from tensortrade.feed.core import Stream, DataFeed, NameSpace
 from tensortrade.oms.exchanges import Exchange
+from tensortrade.oms.instruments import Instrument
 from tensortrade.oms.services.execution.simulated import execute_order
+from tensortrade.oms.instruments import USD, BTC
 from tensortrade.oms.wallets import Wallet, Portfolio
 
+import tensortrade.env.default as default
+from tensortrade.env.default.renderers import PlotlyTradingChart, FileLogger
 
-USD = Instrument("USD", 2, "U.S. Dollar")
-TTC = Instrument("TTC", 8, "TensorTrade Coin")
 
+
+def load_csv(filename):
+    df = pd.read_csv(filename, skiprows=1)
+    df.drop(columns=['symbol', 'volume_btc'], inplace=True)
+    # Fix timestamp form "2019-10-17 09-AM" to "2019-10-17 09-00-00 AM"
+    df['date'] = df['date'].str[:14] + '00-00 ' + df['date'].str[-2:]
+    # Convert the date column type from string to datetime for proper sorting.
+    df['date'] = pd.to_datetime(df['date'])
+    # Make sure historical prices are sorted chronologically, oldest first.
+    df.sort_values(by='date', ascending=True, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    # Format timestamps as you want them to appear on the chart buy/sell marks.
+    df['date'] = df['date'].dt.strftime('%Y-%m-%d %I:%M %p')
+    return df
+
+def main():
+    df = load_csv('data.csv')
+    df.head()
+
+    dataset = ta.add_all_ta_features(df, 'open', 'high', 'low', 'close', 'volume', fillna=True)
+    display(dataset.head(7))
+
+    #Create Chart Price History Data
+    price_history = dataset[['date', 'open', 'high', 'low', 'close', 'volume']]  # chart data
+    dataset.drop(columns=['date', 'open', 'high', 'low', 'close', 'volume'], inplace=True)
+
+    register_env("TradingEnv", create_env)
+
+    analysis = tune.run(
+        "PPO",
+        stop={
+            "episode_reward_mean": 500
+        },
+        config={
+            "env": "TradingEnv",
+            "env_config": {
+                "window_size": 25
+            },
+            "log_level": "DEBUG",
+            "framework": "torch",
+            "ignore_worker_failures": True,
+            "num_workers": 1,
+            "num_gpus": 0,
+            "clip_rewards": True,
+            "lr": 8e-6,
+            "lr_schedule": [
+                [0, 1e-1],
+                [int(1e2), 1e-2],
+                [int(1e3), 1e-3],
+                [int(1e4), 1e-4],
+                [int(1e5), 1e-5],
+                [int(1e6), 1e-6],
+                [int(1e7), 1e-7]
+            ],
+            "gamma": 0,
+            "observation_filter": "MeanStdFilter",
+            "lambda": 0.72,
+            "vf_loss_coeff": 0.5,
+            "entropy_coeff": 0.01
+        },
+        checkpoint_at_end=True
+    )
+
+    # Get checkpoint
+    checkpoints = analysis.get_trial_checkpoints_paths(
+        trial=analysis.get_best_trial("episode_reward_mean"),
+        metric="episode_reward_mean"
+    )
+    checkpoint_path = checkpoints[0][0]
+
+    # Restore agent
+    agent = ppo.PPOTrainer(
+        env="TradingEnv",
+        config={
+            "env_config": {
+                "window_size": 25
+            },
+            "framework": "torch",
+            "log_level": "DEBUG",
+            "ignore_worker_failures": True,
+            "num_workers": 1,
+            "num_gpus": 0,
+            "clip_rewards": True,
+            "lr": 8e-6,
+            "lr_schedule": [
+                [0, 1e-1],
+                [int(1e2), 1e-2],
+                [int(1e3), 1e-3],
+                [int(1e4), 1e-4],
+                [int(1e5), 1e-5],
+                [int(1e6), 1e-6],
+                [int(1e7), 1e-7]
+            ],
+            "gamma": 0,
+            "observation_filter": "MeanStdFilter",
+            "lambda": 0.72,
+            "vf_loss_coeff": 0.5,
+            "entropy_coeff": 0.01
+        }
+    )
+
+    agent.restore(checkpoint_path)
+
+	#Direct Performance and Net Worth Plotting
+    performance = pd.DataFrame.from_dict(env.action_scheme.portfolio.performance, orient='index')
+    performance.plot()
+
+    portfolio.performance.net_worth.plot()
+
+
+#Setup Trading Environment
+##Create Data Feeds
 def create_env(config):
-    x = np.arange(0, 2*np.pi, 2*np.pi / 1000)
-    p = Stream.source(50*np.sin(3*x) + 100, dtype="float").rename("USD-TTC")
+    bitfinex = Exchange("bitfinex", service=execute_order)(
+        Stream.source(price_history['close'].tolist(), dtype="float").rename("USD-BTC")
+    )
 
-    bitfinex = Exchange("bitfinex", service=execute_order)(p)
+    portfolio = Portfolio(USD, [
+        Wallet(bitfinex, 10000 * USD),
+        Wallet(bitfinex, 10 * BTC),
+    ])
 
-    cash = Wallet(bitfinex, 100000 * USD)
-    asset = Wallet(bitfinex, 0 * TTC)
+    with NameSpace("bitfinex"):
+        streams = [
+            Stream.source(dataset[c].tolist(), dtype="float").rename(c) for c in dataset.columns
+        ]
 
-    portfolio = Portfolio(USD, [cash,
-                                asset])
+    feed = DataFeed(streams)
+    display(feed.next())
 
-    feed = DataFeed([p,
-                     p.rolling(window=10).mean().rename("fast"),
-                     p.rolling(window=50).mean().rename("medium"),
-                     p.rolling(window=100).mean().rename("slow"),
-                     p.log().diff().fillna(0).rename("lr")])
+    #Environment with Multiple Renderers
+    chart_renderer = PlotlyTradingChart(
+        display=True,
+        # None for 100% height.
+        height=800,
+        save_format="html",
+        auto_open_html=True,
+    )
 
-    reward_scheme = default.rewards.PBR(price=p)
+    file_logger = FileLogger(
+        # omit or None for automatic file name.
+        filename="example.log",
+        # create a new directory if doesn't exist, None for no directory.
+        path="training_logs"
+    )
 
-    action_scheme = default.actions.BSH(cash=cash,
-                                        asset=asset).attach(reward_scheme)
-    
-    
-    import ray.rllib.agents.ppo as ppo
+    renderer_feed = DataFeed([
+        Stream.source(price_history[c].tolist(), dtype="float").rename(c) for c in price_history]
+    )
 
-# Get checkpoint
-checkpoints = analysis.get_trial_checkpoints_paths(
-    trial=analysis.get_best_trial("episode_reward_mean"),
-    metric="episode_reward_mean"
-)
-checkpoint_path = checkpoints[0][0]
+    env = default.create(
+        portfolio=portfolio,
+        action_scheme="managed-risk",
+        reward_scheme="risk-adjusted",
+        feed=feed,
+        window_size=20,
+        renderer_feed=renderer_feed,
+        renderers=[
+            chart_renderer, 
+            file_logger
+        ]
+    )
 
-# Restore agent
-agent = ppo.PPOTrainer(env="TradingEnv",
-                       config={"env_config": {"window_size": 25},
-                               "framework": "torch",
-                               "log_level": "DEBUG",
-                               "ignore_worker_failures": True,
-                               "num_workers": 1,
-                               "num_gpus": 0,
-                               "clip_rewards": True,
-                               "lr": 8e-6,
-                               "lr_schedule": [[0, 1e-1],
-                                               [int(1e2), 1e-2],
-                                               [int(1e3), 1e-3],
-                                               [int(1e4), 1e-4],
-                                               [int(1e5), 1e-5],
-                                               [int(1e6), 1e-6],
-                                               [int(1e7), 1e-7]],
-                               "gamma": 0,
-                               "observation_filter": "MeanStdFilter",
-                               "lambda": 0.72,
-                               "vf_loss_coeff": 0.5,
-                               "entropy_coeff": 0.01})
-agent.restore(checkpoint_path)
-
-env = default.create(feed=feed,
-                     portfolio=portfolio,
-                     action_scheme=action_scheme,
-                     reward_scheme=reward_scheme,
-                     window_size=config["window_size"],
-                     max_allowed_loss=0.6)
-
-return env
+    return env
 
 
-register_env("TradingEnv", create_env)
 
-analysis = tune.run("PPO",
-                    stop={"episode_reward_mean": 500},
-                    config={"env": "TradingEnv",
-                            "env_config": {"window_size": 25},
-                            "log_level": "DEBUG",
-                            "framework": "torch",
-                            "ignore_worker_failures": True,
-                            "num_workers": 1,
-                            "num_gpus": 0,
-                            "clip_rewards": True,
-                            "lr": 8e-6,
-                            "lr_schedule": [[0, 1e-1],
-                                            [int(1e2), 1e-2],
-                                            [int(1e3), 1e-3],
-                                            [int(1e4), 1e-4],
-                                            [int(1e5), 1e-5],
-                                            [int(1e6), 1e-6],
-                                            [int(1e7), 1e-7]
-                                           ],
-                            "gamma": 0,
-                            "observation_filter": "MeanStdFilter",
-                            "lambda": 0.72,
-                            "vf_loss_coeff": 0.5,
-                            "entropy_coeff": 0.01
-                           },
-                    checkpoint_at_end=True)
+if __name__ == "__main__":
+    main()
