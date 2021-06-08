437871f03d73309b0e0efe0d0878c521bae05986
behnam heydari <behnamheydari.7.21@gmail.com>
2021-06-08 16:27:06 +0430

    Update core.py

--- core.py
+++ core.py
@@ -7,7 +7,6 @@ import pandas as pd
 import ta
 from IPython.display import display
 
-
 from tensortrade.feed.core import Stream, DataFeed, NameSpace
 from tensortrade.oms.exchanges import Exchange
 from tensortrade.oms.instruments import Instrument
@@ -17,7 +16,6 @@ from tensortrade.oms.wallets import Wallet, Portfolio
 
 import tensortrade.env.default as default
 from tensortrade.env.default.renderers import PlotlyTradingChart, FileLogger, ScreenLogger
-
 
 
 def load_csv(filename):
