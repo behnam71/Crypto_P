161c0fc00f2b89208bb55b8beb09ee6251a9b268
behnam heydari <behnamheydari.7.21@gmail.com>
2021-06-19 22:35:51 +0430

    Update continuously_Data.py

--- continuously_Data.py
+++ continuously_Data.py
@@ -2,7 +2,10 @@
 import asyncio
 import os
 import sys
+import pandas as pd
 from pprint import pprint
+
+from ta import add_all_ta_features
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root + '/python')
@@ -12,7 +15,7 @@ import ccxt.async_support as ccxt  # noqa: E402
 
 # this example shows how to fetch OHLCVs continuously, see issue #7498
 # https://github.com/ccxt/ccxt/issues/7498
-async def fetch_ohlcvs_continuously(exchange, timeframe, symbol, fetching_time):
+async def fetch_ohlcvs_continuously(exchange, timeframe, symbol, windows_s, fetching_time):
     print(exchange.id, timeframe, symbol, 'starting')
     all_ohlcvs = []
     limit = 1
@@ -34,33 +37,45 @@ async def fetch_ohlcvs_continuously(exchange, timeframe, symbol, fetching_time):
                 break
             except Exception as e:
                 print(type(e).__name__, e.args, str(e))  # comment if not needed
-                # break # uncomment to break it
-                # pass # uncomment to do nothing and just retry again on next iteration
+                #break | pass - to break it | to do nothing and just retry again on next iteration
                 # or add your own reaction according to the purpose of your app
         print(exchange.id, timeframe, symbol, 'fetched', len(ohlcvs), 'candle(s), time now is', exchange.iso8601(exchange.milliseconds()))
         print(exchange.id, timeframe, symbol, 'all candles:')
         all_ohlcvs += ohlcvs
+        dataset = pd.DataFrame()
+        if len(all_ohlcvs) == windows_s:
+            df = pd.DataFrame(all_ohlcvs, columns =['date', 'open', 'high', 'low', 'close', 'volume'])
+            df.reset_index(drop=True)
+            dataset = add_all_ta_features(df, open="open", high="high", low="low", close="close", volume="volume", fillna=False)
+            dataset = dataset[['date', 'open', 'high', 'low', 'close', 'volume', 'volume_adi', 'volume_obv', 'volume_mfi', 
+                               'volatility_atr', 'volatility_bbm', 'volatility_bbh','volatility_bbl', 'trend_macd', 
+                               'trend_macd_signal', 'trend_macd_diff', 'trend_ema_fast', 'trend_ema_slow', 'trend_adx', 
+                               'trend_adx_pos', 'trend_adx_neg', 'trend_cci', 'trend_ichimoku_conv', 'trend_ichimoku_base', 
+                               'trend_ichimoku_a', 'trend_ichimoku_b', 'trend_psar_up', 'trend_psar_down', 'momentum_rsi', 
+                               'momentum_stoch', 'momentum_stoch_signal', 'momentum_wr']] # chart data
+            dataset.to_csv("data1.csv")
+
         for ohlcv in all_ohlcvs:
-            print(' ', exchange.iso8601(ohlcv[0]), ohlcv[1:])
+            print(" ", exchange.iso8601(ohlcv[0]), ohlcv[1:])
         now = exchange.milliseconds()
     return {symbol: all_ohlcvs}
 
-async def fetch_all_ohlcvs_continuously(loop, exchange_id, timeframe, symbols, fetching_time):
+
+async def fetch_all_ohlcvs_continuously(loop, exchange_id, timeframe, symbols, windows_s, fetching_time):
     exchange_class = getattr(ccxt, exchange_id)
     exchange = exchange_class({'enableRateLimit': True, 'asyncio_loop': loop})
-    input_coroutines = [fetch_ohlcvs_continuously(exchange, timeframe, symbol, fetching_time) for symbol in symbols]
+    input_coroutines = [fetch_ohlcvs_continuously(exchange, timeframe, symbol, windows_s, fetching_time) for symbol in symbols]
     results = await asyncio.gather(*input_coroutines, return_exceptions=True)
     await exchange.close()
     return exchange.extend(*results)
 
-print('CCXT version:', ccxt.__version__)
 
-exchange_id = 'binance'
-symbols = ['ETH/BTC', 'BTC/USDT']
-timeframe = '1m'
-fetching_time = 15 * 60 * 1000 # stop after 15 minutes (approximately 4 iterations)
-loop = asyncio.get_event_loop()
-coroutine = fetch_all_ohlcvs_continuously(loop, exchange_id, timeframe, symbols, fetching_time)
-results = loop.run_until_complete(coroutine)
-pprint(results)
-# results # if you run this code in Jupyter then uncomment thisline to see the output result
+def fetchData(timeframe, symbols, windows_s):
+    print('CCXT version:', ccxt.__version__)
+
+    exchange_id = 'binance'
+    fetching_time = 15 * 60 * 1000 # stop after 15 minutes (approximately 4 iterations)
+    loop = asyncio.get_event_loop()
+    coroutine = fetch_all_ohlcvs_continuously(loop, exchange_id, timeframe, symbols, windows_s, fetching_time)
+    results = loop.run_until_complete(coroutine)
+    pprint(results)
