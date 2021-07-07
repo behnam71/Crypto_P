335150ee8a8e284e60ee1587d73931908c09921e
behnam heydari <behnamheydari.7.21@gmail.com>
2021-07-07 19:04:28 +0430

    Update balance.py

--- balance.py
+++ balance.py
@@ -1,35 +1,26 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+from pprint import pprint
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root + '/python')
 
-import ccxt  # noqa: E402
+import ccxt
 
-
-def style(s, style):
-    return style + s + '\033[0m'
-
-def green(s):
-    return style(s, '\033[92m')
-
-def dump(*args):
-    print(' '.join([str(arg) for arg in args]))
 
 # instantiate exchanges
 binance = ccxt.binance({
     'apiKey': 'SmweB9bNM2qpYkgl4zaQSFPpSzYpyoJ6B3BE9rCm0XYcAdIE0b7n6bm11e8jMwnI',  
     'secret': '8x6LtJztmIeGPZyiJOC7lVfg2ixCUYkhVV7CKVWq2LVlPh8mo3Ab7SMkaC8qTZLt',
 })
-
 binance.urls['api'] = binance.urls['test']  # use the testnet
 
 try:
     # fetch account balance from the exchange
     binanceBalance = binance.fetch_balance()
     # output the result
-    dump(green(binance.name), 'balance', binanceBalance)
+    pprint(binanceBalance)
 
 except ccxt.DDoSProtection as e:
     print(type(e).__name__, e.args, 'DDoS Protection (ignoring)')
